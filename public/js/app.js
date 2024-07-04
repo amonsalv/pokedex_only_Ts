8f2e26e598f9c385d41f10ed6851080a3bb074df
amonsalv <anamonsalve96@gmail.com>
2024-07-04 14:27:08 -0500

    chore: Update app.js with code comments and translations

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 "use strict"; //obligamos que ejecute la funcion en modo estricto
 
 //funcion awaiter para que espere a que se resuelva la promesa antes de seguir con el codigo 
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+let __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -11,11 +11,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 //funcion generadora para que se ejecute el codigo de manera secuencial, es decir, que se ejecute una vez que se haya resuelto la promesa anterior
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+let __generator = (this && this.__generator) || function (thisArg, body) {
+    let _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
     return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
+    function verb(n) { return function (v) { return step([n, v]); }; } //
+    function step(op) { 
         if (f) throw new TypeError("Generator esta ya en ejecucion");
         while (_) try {
             if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
@@ -38,16 +38,16 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var container = document.getElementById("app"); //obtenemos el elemento con el id app
-var pokemons = 100; //numero de pokemons a mostrar
-var fetchData = function () { 
-    for (var i = 1; i <= pokemons; i++) {
+let container = document.getElementById("app"); //obtenemos el elemento con el id app
+let pokemons = 100; //numero de pokemons a mostrar
+let fetchData = function () { 
+    for (let i = 1; i <= pokemons; i++) {
         getPokemon(i);
     }
 };
 //funcion para obtener los datos de la api
-var getPokemon = function (id) { return __awaiter(void 0, void 0, void 0, function () {
-    var data, pokemon, pokemonType, transformedPokemon;
+let getPokemon = function (id) { return __awaiter(void 0, void 0, void 0, function () {
+    let data, pokemon, pokemonType, transformedPokemon;
     return __generator(this, function (_a) { //generador para que se ejecute el codigo de manera secuencial
         switch (_a.label) {
             case 0: return [4 /*yield*/, fetch("https://pokeapi.co/api/v2/pokemon/" + id)];
@@ -71,8 +71,8 @@ var getPokemon = function (id) { return __awaiter(void 0, void 0, void 0, functi
     });
 }); };
 //funcion para mostrar los datos de los pokemons, se crea un template con los datos obtenidos de la api
-var showPokemon = function (pokemon) {
-    var output = "\n<div class=\"card\">\n<span class=\"card--id\">#" + pokemon.id + "</span>\n<img class=\"card--image\" src=" + pokemon.image + " alt=" + pokemon.name + " />\n<h1 class=\"card--name\">" + pokemon.name + "</h1>\n<span class=\"card--details\">" + pokemon.type + "</span>\n</div>\n ";
+let showPokemon = function (pokemon) {
+    let output = "\n<div class=\"card\">\n<span class=\"card--id\">#" + pokemon.id + "</span>\n<img class=\"card--image\" src=" + pokemon.image + " alt=" + pokemon.name + " />\n<h1 class=\"card--name\">" + pokemon.name + "</h1>\n<span class=\"card--details\">" + pokemon.type + "</span>\n</div>\n ";
     container.innerHTML += output;
 };
 //se ejecuta la funcion fetchData
